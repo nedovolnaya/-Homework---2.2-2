940cdd7fa397372aff5a8f84d4525800d57b5c6c
Unteryana <ishtar_v@bk.ru>
2023-05-22 15:22:15 +0300

    HW DONE

---
+++ src/Car.java
@@ -0,0 +1,5 @@
+public class Car extends EngineTransport {
+    public Car(String modelName, int wheelCount) {
+        super(modelName, wheelCount);
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,5 @@
+public class ServiceStation {
+    public void check(Machine machine) {
+        machine.service();
+    }
+}
